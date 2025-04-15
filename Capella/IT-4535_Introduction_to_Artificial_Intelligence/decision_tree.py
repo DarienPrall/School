c1fbe75f483d78642a4391ae4bfdb4157dfe7aa0
Darien <galleon_caboose_0r@icloud.com>
2025-04-14 21:16:49 -0600

    Updated to decision_tree.py
    Initial Commit: Notating out each step

---
+++ Capella/IT-4535_Introduction_to_Artificial_Intelligence/decision_tree.py
@@ -0,0 +1,27 @@
+# STEP 1: Load Libraries
+
+
+
+# STEP 2: Load and Explore Data
+
+
+
+# STEP 3: Split the Data
+
+
+
+# STEP 4: Build Decision Tree
+
+
+
+# STEP 5: Evaluate the Model
+
+
+
+# STEP 6: Visualize the Decision Tree
+
+
+
+# STEP 7: Feature Importance
+
+
