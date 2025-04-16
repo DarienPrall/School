b777ca3f63d467592d70cf1d1cbdf7a54b8552f3
Darien <galleon_caboose_0r@icloud.com>
2025-04-16 08:19:57 -0600

    Update to advance_feature_detection.py
    get_descriptor function created with testing

--- Capella/CSC-4040_Computer_Vision/Assessment_5/advanced_feature_detection.py
+++ Capella/CSC-4040_Computer_Vision/Assessment_5/advanced_feature_detection.py
@@ -8,7 +8,11 @@ matches the keypoints in scene images, and analyzes detection accuracy.
 '''
 
 # Library Imports
+import cv2
+import os
+import numpy as np
 
+from matplotlib import pyplot as plt
 
 
 # --- SECTION 1: Marker Image Descriptor Mapping ---
@@ -19,8 +23,47 @@ To-Do:
 3. Generate keypoints and descriptors using the selected method.
 4. Display and optionally save the keypoint image.
 '''
+def get_descriptor(detector_type):
 
+    # Normalizing the User Entry to Uppercase and Removes Whitespace
+    #For testing
+    #detector_type = input("Please enter the descriptor type (AKAZE, ORB, SIFT, SURF): ")
+    detector_type = detector_type.upper().strip()
 
+    # Check if the input is a string
+    if not isinstance(detector_type, str):
+        raise TypeError("The detector type must be a string. Please enter AKAZE, ORB, SIFT, or SURF.")
+    
+    # Check if the input is empty
+    if not detector_type:
+        raise ValueError("The detector type cannot be empty. Please enter AKAZE, ORB, SIFT, or SURF.")
+
+    if detector_type == 'AKAZE':
+        return cv2.AKAZE_create()
+        #print("AKAZE descriptor selected.")
+    elif detector_type == 'ORB':
+        return cv2.ORB_create()
+        #print("ORB descriptor selected.")
+    elif detector_type == 'SIFT':
+        return cv2.SIFT_create()
+        #print("SIFT descriptor selected.")
+    elif detector_type == 'SURF':
+        return cv2.xfeatures2d.SURF_create()
+        #print("SURF descriptor selected.")
+    else:
+        raise ValueError("The descriptor you selected is not supported. Please choose AKAZE, ORB, SIFT, or SURF.")
+        #print("The descriptor you selected is not supported. Please choose AKAZE, ORB, SIFT, or SURF.")
+    
+# get_descriptor Function Testing
+'''
+- Uncomment the get_descriptor funciton below to test the function
+- Remove detector_type argument when running the script
+- Uncomment line 30
+- Uncomment print statements in flow control
+- Comment out return statements in flow control
+- When finished reverse steps
+'''
+#get_descriptor()
 
 # --- SECTION 2: Scene Image Matching ---
 '''
