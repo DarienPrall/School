b6f3d4d220efa08dbf44f1fdf4ab920d75da7a82
Darien <galleon_caboose_0r@icloud.com>
2025-04-28 09:03:48 -0600

    Assessment_10
    - Folder Creation
    - Step 1 Completed

---
+++ Capella/CSC-4030_Introduction_to_Machine_Learning/Assessment_10/cnn_using_keras.py
@@ -0,0 +1,16 @@
+#---------------------------------------------------------
+# Instructions
+# 1. Install TensorFlow
+# 2.Import the necessary libraries
+# 3. Load the Fashion MNIST dataset
+# 4. Preprocess the data
+# 5. Build 3 different CNN models
+# 6. Compile the models
+# 7. Train the models
+# 8. Evaluate the models
+# 9. Compare and Conclude
+#---------------------------------------------------------
+
+# 1. Install TensorFlow
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
