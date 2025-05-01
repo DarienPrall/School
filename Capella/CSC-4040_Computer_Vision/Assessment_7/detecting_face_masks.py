306393992d78fc2b477c7973cf359bbcdef23707
Darien <galleon_caboose_0r@icloud.com>
2025-05-01 08:15:16 -0600

    Assessment_7 Setup

---
+++ Capella/CSC-4040_Computer_Vision/Assessment_7/detecting_face_masks.py
@@ -0,0 +1,32 @@
+# DETECTING FACE MASKS
+# - This program will be used for detecting face masks (mask/no mask) for building entry control. 
+# - This is useful for personal, commercial, or industrial settings.
+# DATASET CITATION
+'''
+Face Mask Detection. (2020). Larxel/CC0: Public Domain. Kaggle.
+
+https://www.kaggle.com/datasets/andrewmvd/face-mask-detection
+'''
+
+# Install Required Libraries
+
+
+# Import Libraries
+
+
+# Load Pre-trained Classifier (Haar Cascade Classifier)
+
+
+# Load Image (Sample)
+
+
+# Detect Faces
+
+
+# Draw Bounding Boxes
+
+
+# Display Detected Faces
+
+
+# Save Detected Image
