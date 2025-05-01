f4cd3bd154b539fc20a91d80dbdd4546a51b68c8
Darien <galleon_caboose_0r@icloud.com>
2025-05-01 10:13:20 -0600

    Update 3
    - Setup manifest
    - defined manifest load

--- Capella/CSC-4040_Computer_Vision/Assessment_7/detecting_facemasks.py
+++ Capella/CSC-4040_Computer_Vision/Assessment_7/detecting_facemasks.py
@@ -1,32 +1,60 @@
 # DETECTING FACE MASKS
 # - This program will be used for detecting face masks (mask/no mask) for building entry control. 
 # - This is useful for personal, commercial, or industrial settings.
-# DATASET CITATION
 '''
-Face Mask Detection. (2020). Larxel/CC0: Public Domain. Kaggle.
+DATASET SOURCE
+All photos used in this assessment are personal photos taken by Darien Prall
+'''
 
-https://www.kaggle.com/datasets/andrewmvd/face-mask-detection
-'''
 # Import Libraries
 import os
-import shutil
-import cv2
 
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array
+# Paths
+masks_directory = ('/Users/darienprall/Documents/GitHub/School/Capella/CSC-4040_Computer_Vision/Assessment_7/images/outputs/masks')
+no_masks_directory = ('/Users/darienprall/Documents/GitHub/School/Capella/CSC-4040_Computer_Vision/Assessment_7/images/outputs/no_masks')
+raw_photos_directory = ('/Users/darienprall/Documents/GitHub/School/Capella/CSC-4040_Computer_Vision/Assessment_7/images/raw_photos')
+raw_photos_manifest = ('/Users/darienprall/Documents/GitHub/School/Capella/CSC-4040_Computer_Vision/Assessment_7/images/raw_photos/raw_photos_manifest.txt')
 
-# Define Paths
-images_directory = ('/Users/darienprall/Documents/GitHub/School/Capella/CSC-4040_Computer_Vision/Assessment_7/archive/images')
-output_directory = ('/Users/darienprall/Documents/GitHub/School/Capella/CSC-4040_Computer_Vision/Assessment_7/archive/images/outputs')
+# Manifest
+def load_manifest(manifest_path):
+    
+    # Define Arrays
+    masks = []
+    no_masks = []
 
-# Define Output Folders
-mask_directory = os.path.join(output_directory, 'Masks')
-no_mask_directory = os.path.join(output_directory, 'No Masks')
-os.makedirs(mask_directory, exist_ok = True)
-os.makedirs(no_mask_directory, exist_ok = True)
+    # Open path
+    with open(manifest_path, 'r') as file:
+        
+        # Define Navigator
+        current_section = None
 
-# Load face mask detector and mask detector model
+        # Loop through every line
+        for line in file:
 
+            # Strip Whitespace
+            line = line.strip()
 
-# Function to predict mask status
+            # Check if line is FACE MASK (As set up in the manifest)
+            if line == "FACE MASK":
+                current_section = 'face_mask'
+            # Check if line is NO FACE MASK (As set up in the manifest)
+            elif line == "NO FACE MASK":
+                current_section = 'no_face_mask'
 
+            elif line:
+
+                # Append Mask files to array
+                if current_section == 'face_mask':
+                    masks.append(line)
+                
+                # Append No Face Mask to arry
+                elif current_section == 'no_face_mask':
+                    no_masks.append(line)
+
+    # Print the content for review
+    # print(f"Masks: {masks}")
+    # print(f"No Masks: {no_masks}")
+    return masks, no_masks
+
+# Test load_manifest
+# load_manifest(raw_photos_manifest)
